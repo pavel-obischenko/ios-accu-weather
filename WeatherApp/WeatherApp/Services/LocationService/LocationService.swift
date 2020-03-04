bd783c188f27d2979e9a85e3e6fba93084a66a07
Pavel Obischenko <pavel.obischenko@gmail.com>
2020-03-04 20:16:47 +0300

    several files added

---
+++ WeatherApp/WeatherApp/Services/LocationService/LocationService.swift
@@ -0,0 +1,13 @@
+//
+//  LocationService.swift
+//  WeatherApp
+//
+//  Created by Pavel Obischenko on 26.02.2020.
+//  Copyright © 2020 Pavel Obischenko. All rights reserved.
+//
+
+import Foundation
+
+class LocationService {
+    
+}
